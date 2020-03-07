9683b47f237647694013d0a74dd8f31ef8357974
wilfar <fabian.arenas@realpage.com>
2020-03-07 12:48:09 -0500

    Many changes, was not working... now works

---
+++ TestAngular/Infraestructure/Migrations/20200307171534_Creation.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infraestructure.Migrations
+{
+    public partial class Creation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "AvailableQuantity",
+                table: "Products",
+                nullable: true,
+                oldClrType: typeof(int));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "AvailableQuantity",
+                table: "Products",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldNullable: true);
+        }
+    }
+}
